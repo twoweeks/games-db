54275bbf1c4c0f4c65e7887e02598470298cdb38
Dmitry <team@cojam.ru>
2017-01-18 23:19:49 +0500

    Readability and optimization

---
+++ assets/script.js
@@ -0,0 +1,174 @@
+'use strict';
+
+function genFa(a, b, c, d) {
+	let type = !a ? 'question' : a,	content = !b ? '' : b,	cls = !c ? '' : ' ' + c, check2x = (!d && d !== !1) ? ' fa-2x' : '';
+	return '<i class="fa fa-' + type + check2x + cls + '" aria-hidden="true">' + content + '</i>';
+}
+
+function postlinks(el, obj, addtext) {
+	if (!obj) return;
+	addtext = $(addtext).addClass('fa-space');
+	if (obj.yadisk) el.append('<a href="https://yadi.sk/d/' + obj.yadisk + '" target="_blank" class="mui-btn mui-btn--raised dl-link"></a>');
+	if (obj.gdrive) el.append('<a href="https://docs.google.com/uc?id=' + obj.gdrive + '&export=download" target="_blank" class="mui-btn mui-btn--raised dl-link"></a>');
+	if (obj.web) el.append('<a href="' + obj.web + '" target="_blank" class="mui-btn mui-btn--raised" rel="nofollow noopener">' + genFa('share', '', 'download-icon') + '</a>');
+	if (obj.github) el.append('<a href="https://github.com/' + obj.github + '" target="_blank" class="mui-btn mui-btn--raised">' + genFa('github', '', 'download-icon') + '</a>');
+	if (obj.gplay) el.append('<a href="https://play.google.com/store/apps/details?id=' + obj.gplay + '" target="_blank" class="mui-btn mui-btn--raised">' + genFa('google', '', 'download-icon') + '</a>');
+	if (obj.itunes) el.append('<a href="https://itunes.apple.com/app/id' + obj.itunes + '" target="_blank" class="mui-btn mui-btn--raised">' + genFa('apple', '', 'download-icon') + '</a>');
+	if (obj.yadisk || obj.gdrive || obj.web || obj.github || obj.gplay || obj.itunes) el.append(addtext);
+}
+
+const infoCDN = 'https://cdn.rawgit.com/twoweeks/db/master/', imgCDN = 'https://113217.selcdn.ru/gd/';
+
+document.addEventListener('DOMContentLoaded', function() {
+	let compBtn = document.querySelectorAll('.getCompBtn');
+
+	for (let i = 0; i < compBtn.length; i++) {
+		compBtn[i].addEventListener('click', function(e) {
+			e.preventDefault();
+			getComps(this.dataset.comp);
+		});
+	}
+
+	getComps('twg.json');
+
+	smoothScroll.init({
+		selector: '[data-scroll]',
+		speed: 1000,
+		easing: 'easeInOutCubic',
+		offset: 0,
+		updateURL: false
+	});
+});
+
+function getComps(comp){
+	$.getJSON(infoCDN + comp, function(result){
+		var reskeys = Object.keys(result),
+		compsList = document.querySelector('#sidedrawer .comps-list'),
+		compsContainer = document.querySelector('.comps-container');
+
+		compsList.innerHTML = '';
+		compsContainer.innerHTML = '';
+
+		for (let i = 0; i < reskeys.length; i++) {
+			let res = Object.create(result[reskeys[i]]);
+
+			if (res.coalition) {
+				reskeys[i] += '-1';
+				reskeys.splice(i + 1, 0, i + 1 + '-2');
+				result[reskeys[i + 1]] = res.glowstick;
+				result[reskeys[i]] = res.coalition;
+				result[reskeys[i]].note = res.note;
+			}
+
+			let name = 'Конкурс №' + reskeys[i];
+			compsList.innerHTML += '<li><a class="mui-btn mui-btn--raised" href="#comp' + reskeys[i] + '" data-scroll>' + name + ' </a></li>';
+		}
+
+		var b = $('.comps-container');
+
+		for (let i = 0; i < reskeys.length; i++){
+			let twg = result[reskeys[i]];
+			b.append('<h1 id="comp' + reskeys[i] + '" class="comp-number">Конкурс №' + reskeys[i] + '</h1>');
+
+			let compEl = $('<div class="mui-panel comp-header"></div>').appendTo(b);
+
+			if (twg.themes) {
+				for (let i = 1; i < twg.themes.same.length; i++) {
+					compEl.append('<h1><i class="fa fa-tag" aria-hidden="true"></i> ' + twg.themes.same[i] + '</h1>');
+					if (twg.themes.descriptions) compEl.append(twg.themes.descriptions[i] == '' ? '<p>Описания нет.</p>' : '<p class="game-text">' + twg.themes.descriptions[i] + '</p>');
+				}
+			} else compEl.append('<h1><i class="fa fa-tag" aria-hidden="true"></i> ' + twg.theme + '</h1>');
+
+			if (twg.description) compEl.append(twg.description + '<br>');
+
+			if (twg.achievements) {
+				let achievements = $('<details><summary class="mui-btn mui-btn--raised">Ачивки</summary></details>').appendTo(compEl);
+				achievements = $('<div class="mui--z1 game-achievements"></div>').appendTo(achievements);
+
+				for (let i = 0; i < twg.achievements.same.length; i++) {
+					achievements.append('<h4><i class="fa fa-star" aria-hidden="true"></i> ' + twg.achievements.same[i] + '</h4>');
+					if (twg.achievements.descriptions) achievements.append(twg.achievements.descriptions[i] == '' ? '<p>Описания нет.</p>' : '<p class="game-text">' + twg.achievements.descriptions[i] + '</p>');
+				}
+			}
+
+			if (twg.note) compEl.append('<pre>Примечание: ' + twg.note + '</pre><br>');
+
+			compEl.append('<br><pre>Начало конкурса: ' + new moment(twg.start, 'X').format('LLL') + '<br>Конец конкурса: ' + new moment(twg.end, 'X').format('LLL') + '</pre><br>');
+
+			postlinks(compEl, twg.archive);
+
+			b.append('<div class="mui--z1 comp-games"></div>');
+
+			for (let i = 0; i < twg.games.length; i++) {
+				let game = twg.games[i];
+
+				if (game.status == 'disqualified' && i + 2 < twg.games.length) {
+					twg.games.push(game);
+					continue;
+				}
+
+				let gameEl = $('<div class="mui-panel"></div>').appendTo(b.children().last()), gameBodyEl = $('<div class="game-body"></div>');
+
+				if (game.image) gameEl.append('<img src="' + imgCDN + game.image + '" class="game-img" onerror="$(this).next().addClass("game-body-full"); $(this).remove();" onclick="window.open(this.src)">')
+				else gameBodyEl.addClass('game-body-full');
+
+				gameBodyEl.appendTo(gameEl);
+
+				if (game.status != 'disqualified') gameBodyEl.append(genFa('trophy', '', 'mui--pull-right game-status-' + game.status));
+				else gameBodyEl.append(genFa('ban', '', 'mui--pull-right game-status-' + game.status))
+				gameBodyEl.append('<h2 class="game-header">' + genFa('gamepad', '', '', !1) + ' ' + game.name + '</h2>');
+
+				if (game.genre) gameBodyEl.append('<br><pre class="game-text">Жанр: ' + game.genre + '</pre>');
+				gameBodyEl.append(game.description === undefined ? '<p>Описания нет.</p>' : '<p class="game-text">' + game.description + '</p>');
+
+				if (game.note) gameBodyEl.append('<pre>Примечание: ' + game.note + '</pre><br>');
+
+				let downloads = $('<div class="game-downloads"></div>').appendTo(gameBodyEl);
+
+				if (game.other_links) {
+					if (game.other_links.repo) {
+						postlinks(downloads, game.other_links.repo);
+						downloads.append('<div class="mui-divider"></div>');
+					}
+					if (game.other_links.updated) {
+						postlinks(downloads, game.other_links.updated, genFa('check') + genFa('arrow-circle-up') + '<br>');
+						downloads.append('<div class="mui-divider"></div>');
+					}
+				}
+
+				postlinks(downloads, game.links, genFa('check') + '<br>');
+
+				if (game.other_links) {
+					if (game.other_links.final_multi) {
+						downloads.append('<div class="mui-divider"></div>');
+						postlinks(downloads, game.other_links.final_multi.win, genFa('windows') + '<br>');
+						postlinks(downloads, game.other_links.final_multi.win_x64, genFa('windows', '64') + '<br>');
+						postlinks(downloads, game.other_links.final_multi.linux, genFa('linux') + '<br>');
+						postlinks(downloads, game.other_links.final_multi.mac, genFa('apple') + genFa('desktop') + '<br>');
+						postlinks(downloads, game.other_links.final_multi.android, genFa('android') + '<br>');
+					}
+					if (game.other_links.store) {
+						downloads.append('<div class="mui-divider"></div>');
+						postlinks(downloads, game.other_links.store, genFa('mobile') + '<br>');
+					}
+					if (game.other_links.demo_multi) {
+						downloads.append('<div class="mui-divider"></div>');
+						postlinks(downloads, game.other_links.demo_multi.win, genFa('bug') + genFa('windows') + '<br>');
+						postlinks(downloads, game.other_links.demo_multi.win_x64, genFa('bug') + genFa('windows') + '<br>');
+						postlinks(downloads, game.other_links.demo_multi.linux, genFa('bug') + genFa('linux') + '<br>');
+						postlinks(downloads, game.other_links.demo_multi.mac, genFa('bug') + genFa('apple') + genFa('desktop') + '<br>');
+						postlinks(downloads, game.other_links.demo_multi.android, genFa('bug') + genFa('android') + '<br>');
+					}
+					if (game.other_links.demo_updated) {
+						downloads.append('<div class="mui-divider"></div>');
+						postlinks(downloads, game.other_links.demo_updated, genFa('bug') + genFa('arrow-circle-up') + '<br>');
+					}
+					if (game.other_links.demo) {
+						downloads.append('<div class="mui-divider"></div>');
+						postlinks(downloads, game.other_links.demo, genFa('bug') + '<br>');
+					}
+				}
+			}
+		}
+	});
+}
