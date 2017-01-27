b97c883bf426005573d66ea8d75ec5e758bbfee8
Dmitry <team@cojam.ru>
2017-01-27 17:08:44 +0500

    Small restyling

--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,14 @@ function genFa(a, b, c, d) {
 	return '<i class="fa fa-' + type + check2x + cls + '" aria-hidden="true">' + content + '</i>';
 }
 
-function _create(a, b) {
+function genDlLink(a, b, c) {
+	let link = !a ? 'javascript:void(0);' : a,	content = !b ? '' : b,	cls = !c ? '' : ' ' + c;
+	return '<a href="' + link + '" target="_blank" class="mui-btn mui-btn--raised ' + cls + '" rel="nofollow noopener">' + content + '</a>';
+}
+
+function genElem(a, b) {
 	let elem = document.createElement(a);
-	if (b) elem.classList.add(b);
+	if (b) elem.setAttribute('class', b);
 	return elem;
 }
 
@@ -17,25 +22,26 @@ function parseHTML(str) {
   return tmp.body.children;
 };
 
-function postlinks(el, obj, addtext) {
+function postLinks(el, obj, addtext) {
 	if (!obj) return;
 	addtext = parseHTML(addtext);
-	if(addtext[0]) addtext[0].classList.add("fa-space");
-	if (obj.yadisk) el.innerHTML += '<a href="https://yadi.sk/d/' + obj.yadisk + '" target="_blank" class="mui-btn mui-btn--raised dl-link"></a>';
-	if (obj.gdrive) el.innerHTML += '<a href="https://docs.google.com/uc?id=' + obj.gdrive + '&export=download" target="_blank" class="mui-btn mui-btn--raised dl-link"></a>';
-	if (obj.web) el.innerHTML += '<a href="' + obj.web + '" target="_blank" class="mui-btn mui-btn--raised" rel="nofollow noopener">' + genFa('share', '', 'download-icon') + '</a>';
-	if (obj.github) el.innerHTML += '<a href="https://github.com/' + obj.github + '" target="_blank" class="mui-btn mui-btn--raised">' + genFa('github', '', 'download-icon') + '</a>';
-	if (obj.gplay) el.innerHTML += '<a href="https://play.google.com/store/apps/details?id=' + obj.gplay + '" target="_blank" class="mui-btn mui-btn--raised">' + genFa('google', '', 'download-icon') + '</a>';
-	if (obj.itunes) el.innerHTML += '<a href="https://itunes.apple.com/app/id' + obj.itunes + '" target="_blank" class="mui-btn mui-btn--raised">' + genFa('apple', '', 'download-icon') + '</a>';
-	if ((obj.yadisk || obj.gdrive || obj.web || obj.github || obj.gplay || obj.itunes) && addtext) for(let i of addtext) el.innerHTML += i.outerHTML;
+	if (addtext[0]) addtext[0].classList.add("fa-space");
+	if (obj.yadisk) el.innerHTML += genDlLink('https://yadi.sk/d/' + obj.yadisk, '', 'dl-link');
+	if (obj.gdrive) el.innerHTML += genDlLink('https://docs.google.com/uc?id=' + obj.gdrive, '', 'dl-link');
+	if (obj.web) el.innerHTML += genDlLink(obj.web, genFa('share', '', 'download-icon'));
+	if (obj.github) el.innerHTML += genDlLink('https://github.com/' + obj.github, genFa('github', '', 'download-icon'));
+	if (obj.gplay) el.innerHTML += genDlLink('https://play.google.com/store/apps/details?id=' + obj.gplay, genFa('google', '', 'download-icon'));
+	if (obj.itunes) el.innerHTML += genDlLink('https://itunes.apple.com/app/id' + obj.itunes, genFa('apple', '', 'download-icon'));;
+	if ((obj.yadisk || obj.gdrive || obj.web || obj.github || obj.gplay || obj.itunes) && addtext) for (let i of addtext) el.innerHTML += i.outerHTML;
 }
 
 const infoCDN = 'https://raw.githubusercontent.com/twoweeks/db/master/', imgCDN = 'https://113217.selcdn.ru/gd/';
 
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
 	let compBtn = document.querySelectorAll('.getCompBtn');
 
 	for (let i = 0; i < compBtn.length; i++) {
+		compBtn[i].setAttribute('href', 'javascript:void(0);');
 		compBtn[i].addEventListener('click', function(e) {
 			e.preventDefault();
 			getComps(this.dataset.comp);
@@ -48,19 +54,22 @@ document.addEventListener('DOMContentLoaded', ()=>{
 		selector: '[data-scroll]',
 		speed: 1000,
 		easing: 'easeInOutCubic',
-		offset: 0,
-		updateURL: false
+		offset: 0
 	});
 });
 
 function getComps(comp) {
-	fetch(infoCDN + comp).then((response)=>{
-		return response.json();
-	}).then((result)=>{
-		let reskeys = Object.keys(result), compsList = document.querySelector('#sidedrawer .comps-list'), compsContainer = document.querySelector('.comps-container');
+	history.pushState('', document.title, window.location.pathname);
 
-		compsList.innerHTML = '';
-		compsContainer.innerHTML = '';
+	let compsList = document.querySelector('.comps-list'), compsContainer = document.querySelector('.comps-container');
+
+	compsList.innerHTML = '';
+	compsContainer.innerHTML = '';
+
+	fetch(infoCDN + comp).then((response) => {
+		return response.json();
+	}).then((result) => {
+		let reskeys = Object.keys(result);
 
 		for (let i = 0; i < reskeys.length; i++) {
 			let res = Object.create(result[reskeys[i]]);
@@ -79,14 +88,14 @@ function getComps(comp) {
 
 		let b = $('.comps-container'); // @TODO vanilla here, pls
 
-		for (let i = 0; i < reskeys.length; i++){
+		for (let i = 0; i < reskeys.length; i++) {
 			let twg = result[reskeys[i]];
 			b.append('<h1 id="comp' + reskeys[i] + '" class="comp-number">Конкурс №' + reskeys[i] + '</h1>');
 
 			let compEl = $('<div class="mui-panel comp-header"></div>').appendTo(b);
 
 			if (twg.themes) {
-				for (let i = 1; i < twg.themes.same.length; i++) {
+				for (let i = 0; i < twg.themes.same.length; i++) {
 					compEl.append('<h1>' + genFa('tag', '', '', !1) + ' ' + twg.themes.same[i] + '</h1>');
 					if (twg.themes.descriptions) compEl.append(twg.themes.descriptions[i] == '' ? '<p>Описания нет.</p>' : '<p class="game-text">' + twg.themes.descriptions[i] + '</p>');
 				}
@@ -108,7 +117,7 @@ function getComps(comp) {
 
 			compEl.append('<br><pre>Начало конкурса: ' + new moment(twg.start, 'X').format('LLL') + '<br>Конец конкурса: ' + new moment(twg.end, 'X').format('LLL') + '</pre><br>');
 
-			postlinks(compEl, twg.archive);
+			postLinks(compEl, twg.archive);
 
 			b.append('<div class="mui--z1 comp-games"></div>');
 
@@ -120,7 +129,7 @@ function getComps(comp) {
 					continue;
 				}
 
-				let gameEl = $('<div class="mui-panel"></div>').appendTo(b.children().last()), gameBodyEl = $('<div class="game-body"></div>'), gameImage = _create('img', 'game-img'), gameStatus;
+				let gameEl = $('<div class="mui-panel"></div>').appendTo(b.children().last()), gameBodyEl = $('<div class="game-body"></div>'), gameImage = genElem('img', 'game-img'), gameStatus;
 
 				switch (game.image) {
 					case '':
@@ -138,6 +147,7 @@ function getComps(comp) {
 				switch (game.status) {
 					case 'disqualified':
 						gameStatus = 'Дисквалификация';
+						gameBodyEl.append('<span title="' + gameStatus + '">' + genFa('ban', '', 'mui--pull-right game-status-' + game.status) + '</span>')
 						break;
 					case 'win':
 					case '1':
@@ -154,11 +164,10 @@ function getComps(comp) {
 				}
 
 				if (game.status != 'disqualified') gameBodyEl.append('<span title="' + gameStatus + '">' + genFa('trophy', '', 'mui--pull-right game-status-' + game.status) + '</span>');
-				else gameBodyEl.append(genFa('ban', '', 'mui--pull-right game-status-' + game.status))
 				gameBodyEl.append('<h2 class="game-header">' + genFa('gamepad', '', '', !1) + ' ' + game.name + '</h2>');
 
 				if (game.genre) gameBodyEl.append('<br><pre class="game-text">Жанр: ' + game.genre + '</pre>');
-				gameBodyEl.append(game.description === undefined ? '<p>Описания нет.</p>' : '<p class="game-text">' + game.description + '</p>');
+				gameBodyEl.append(game.description === undefined ? '<p>Описания нет.</p>' : '<p class="game-text">' + game.description.replace(/\n/g, "<br>") + '</p>');
 
 				if (game.note) gameBodyEl.append('<pre>Примечание: ' + game.note + '</pre><br>');
 
@@ -167,48 +176,61 @@ function getComps(comp) {
 
 				if (game.other_links) {
 					if (game.other_links.repo) {
-						postlinks(downloads, game.other_links.repo);
+						postLinks(downloads, game.other_links.repo);
+						downloads.innerHTML += '<div class="mui-divider"></div>';
+					}
+					if (game.other_links.gamejolt) {
+						downloads.innerHTML += genDlLink('http://gamejolt.com/games/' + game.other_links.gamejolt, '', 'dl-link') + genFa('desktop', '', 'fa-space');
 						downloads.innerHTML += '<div class="mui-divider"></div>';
 					}
 					if (game.other_links.updated) {
-						postlinks(downloads, game.other_links.updated, genFa('check') + genFa('arrow-circle-up') + '<br>');
+						postLinks(downloads, game.other_links.updated, genFa('check') + genFa('arrow-circle-up') + '<br>');
 						downloads.innerHTML += '<div class="mui-divider"></div>';
 					}
 				}
 
-				postlinks(downloads, game.links, genFa('check') + '<br>');
+				postLinks(downloads, game.links, genFa('check') + '<br>');
 
 				if (game.other_links) {
 					if (game.other_links.final_multi) {
 						downloads.innerHTML += '<div class="mui-divider"></div>';
-						postlinks(downloads, game.other_links.final_multi.win, genFa('windows') + '<br>');
-						postlinks(downloads, game.other_links.final_multi.win_x64, genFa('windows', '64') + '<br>');
-						postlinks(downloads, game.other_links.final_multi.linux, genFa('linux') + '<br>');
-						postlinks(downloads, game.other_links.final_multi.mac, genFa('apple') + genFa('desktop') + '<br>');
-						postlinks(downloads, game.other_links.final_multi.android, genFa('android') + '<br>');
+						postLinks(downloads, game.other_links.final_multi.win, genFa('windows') + '<br>');
+						postLinks(downloads, game.other_links.final_multi.win_x64, genFa('windows', '64') + '<br>');
+						postLinks(downloads, game.other_links.final_multi.linux, genFa('linux') + '<br>');
+						postLinks(downloads, game.other_links.final_multi.mac, genFa('apple') + genFa('desktop') + '<br>');
+						postLinks(downloads, game.other_links.final_multi.android, genFa('android') + '<br>');
 					}
 					if (game.other_links.store) {
 						downloads.innerHTML += '<div class="mui-divider"></div>';
-						postlinks(downloads, game.other_links.store, genFa('mobile') + '<br>');
+						postLinks(downloads, game.other_links.store, genFa('mobile') + '<br>');
 					}
 					if (game.other_links.demo_multi) {
 						downloads.innerHTML += '<div class="mui-divider"></div>';
-						postlinks(downloads, game.other_links.demo_multi.win, genFa('bug') + genFa('windows') + '<br>');
-						postlinks(downloads, game.other_links.demo_multi.win_x64, genFa('bug') + genFa('windows') + '<br>');
-						postlinks(downloads, game.other_links.demo_multi.linux, genFa('bug') + genFa('linux') + '<br>');
-						postlinks(downloads, game.other_links.demo_multi.mac, genFa('bug') + genFa('apple') + genFa('desktop') + '<br>');
-						postlinks(downloads, game.other_links.demo_multi.android, genFa('bug') + genFa('android') + '<br>');
+						postLinks(downloads, game.other_links.demo_multi.win, genFa('bug') + genFa('windows') + '<br>');
+						postLinks(downloads, game.other_links.demo_multi.win_x64, genFa('bug') + genFa('windows') + '<br>');
+						postLinks(downloads, game.other_links.demo_multi.linux, genFa('bug') + genFa('linux') + '<br>');
+						postLinks(downloads, game.other_links.demo_multi.mac, genFa('bug') + genFa('apple') + genFa('desktop') + '<br>');
+						postLinks(downloads, game.other_links.demo_multi.android, genFa('bug') + genFa('android') + '<br>');
 					}
 					if (game.other_links.demo_updated) {
 						downloads.innerHTML += '<div class="mui-divider"></div>';
-						postlinks(downloads, game.other_links.demo_updated, genFa('bug') + genFa('arrow-circle-up') + '<br>');
+						postLinks(downloads, game.other_links.demo_updated, genFa('bug') + genFa('arrow-circle-up') + '<br>');
 					}
 					if (game.other_links.demo) {
 						downloads.innerHTML += '<div class="mui-divider"></div>';
-						postlinks(downloads, game.other_links.demo, genFa('bug') + '<br>');
+						postLinks(downloads, game.other_links.demo, genFa('bug') + '<br>');
 					}
 				}
 			}
 		}
+	}).catch((e) => {
+		let loadingElem = document.querySelector('.loading');
+
+		if (!loadingElem) {
+			loadingElem = genElem('p', 'loading');
+			compsContainer.appendChild(loadingElem);
+		}
+
+		loadingElem.textContent = 'Сервер GitHub сейчас недоступен.';
 	});
 }
